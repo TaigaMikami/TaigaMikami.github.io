e793361d0049abae7457eb47546e432280be9b8c
TaigaMikami <mikamimikami60@gmail.com>
2019-11-03 02:45:34 +0900

    remove previous hp add flutter portofolio

---
+++ portfolio_flutter/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "TaigaMikami's Portofolio",
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("TaigaMikami's Portofolio"),
+        ),
+        body: Center(
+          child: Text("Coming Soon"),
+        ),
+      ),
+    );
+  }
+}
